4fe43a4f878e0f8514b1f50b5e2c615516c27adc
Melisa <melamachia@gmail.com>
2024-06-10 05:13:30 -0700

    first commit

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+
+
+const btn = document.getElementById('btn-btn')
+btn.onclick = function(){
+    console.log('button has been clicked')
+    alert('Girly priorities gotchu')
+}
+
+const btn2 = document.getElementById('btn-btn-2')
+btn2.onclick = function(){
+    console.log('button has been clicked')
+    alert('Girly priorities gotchu')
+}
